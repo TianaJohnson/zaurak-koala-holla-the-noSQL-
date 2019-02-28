5461d614e91ba78b293c605e539c141c09cb5c58
Julie Zembik <kurokurosan@gmail.com>
2019-02-28 13:52:00 -0600

    added react-redux to project

--- src/components/KoalaTable/KoalaTable.js
+++ src/components/KoalaTable/KoalaTable.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class KoalaTable extends Component {
     render() {
         return (
             <div>Table goes here!</div>
+
         );
     }
 }
