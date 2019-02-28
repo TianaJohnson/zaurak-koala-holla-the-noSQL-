78fca432c3b5e3ccfea0cc6437de9713a52a908a
James Tucker <jamesctucker@protonmail.com>
2019-02-28 14:13:42 -0600

    resolved form errors

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+
 const sagaMiddleware = createSagaMiddleware();
 
-import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
-import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
+
 
 
 const middlewareList = process.env.NODE_ENV === 'development' ?
@@ -19,10 +19,10 @@ const middlewareList = process.env.NODE_ENV === 'development' ?
 const store = createStore(
     // tells the saga middleware to use the rootReducer
     // rootSaga contains all of our other reducers
-    rootReducer,
     // adds all middleware to our project including saga and logger
     applyMiddleware(...middlewareList),
 );
+
 
 
 ReactDOM.render(
